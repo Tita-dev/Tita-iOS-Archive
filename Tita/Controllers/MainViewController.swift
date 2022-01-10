4c4498729e095ce3cc954e50d74fdcdf78fec2f4
hyein0112 <bhi12134@gmail.com>
2022-01-10 20:24:07 +0900

    [CREATE] 기본세팅

--- Tita/Controllers/MainViewController.swift
+++ Tita/Controllers/MainViewController.swift
@@ -6,13 +6,71 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class MainViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureUI()
     }
-
-
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
+    }
+    
+    // MARK: - Add View
+    
+    private func addView(){
+        
+    }
+    
+    // MARK: - Corner Radius
+    
+    private func cornerRadius(){
+        
+    }
+    
+    // MARK: - Location
+    
+    private func location(){
+        
+    }
+    
 }
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct MainViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        MainViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct MainControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            MainViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
