a53cc45703d8f07ec08ccd3a9f96734461b79298
hyein0112 <bhi12134@gmail.com>
2022-01-10 20:22:08 +0900

    [CREATE] MainViewController

---
+++ Tita/Controllers/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/01/10.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
