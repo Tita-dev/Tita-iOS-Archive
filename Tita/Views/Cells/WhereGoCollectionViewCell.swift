f3a64eead0a6c17524b8ec49f4866f7fcf73ceb0
hyein0112 <bhi12134@gmail.com>
2022-01-16 00:28:02 +0900

    [UPDATE] 기본세팅

--- Tita/Views/Cells/WhereGoCollectionViewCell.swift
+++ Tita/Views/Cells/WhereGoCollectionViewCell.swift
@@ -10,5 +10,30 @@ import SnapKit
 import Then
 
 class WhereGoCollectionViewCell: UICollectionViewCell {
+    //MARK: - property
+
     
+    //MARK: - lifeCycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addView()
+        location()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - addView
+    private func addView() {
+        
+    }
+        
+    // MARK: - location
+    private func location() {
+        
+    }
 }
+
+
