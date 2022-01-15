90b38f305fc07cf39a4b8138c11a5450b9c299a8
hyein0112 <bhi12134@gmail.com>
2022-01-15 17:02:56 +0900

    [CREATE] WhereGoCollectionCell

---
+++ Tita/Views/Cells/WhereGoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WhereGoCollectionViewCell.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/01/15.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class WhereGoCollectionViewCell: UICollectionViewCell {
+    
+}
