f8928ba03ef5151d140792f4a9c726cad694267d
노연주 <yjnoh1016@naver.com>
2022-01-18 17:37:30 +0900

    [UPDATE] 행간 조정 Extension 추가

--- Tita/Extension/Extension.swift
+++ Tita/Extension/Extension.swift
@@ -329,3 +329,19 @@ extension UITextField {
     }
 }
 
+// MARK: - Line Spacing
+
+extension NSAttributedString {
+    func withLineSpacing(_ spacing: CGFloat) -> NSAttributedString {
+
+
+        let attributedString = NSMutableAttributedString(attributedString: self)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        paragraphStyle.lineSpacing = spacing
+        attributedString.addAttribute(.paragraphStyle,
+                                      value: paragraphStyle,
+                                      range: NSRange(location: 0, length: string.count))
+        return NSAttributedString(attributedString: attributedString)
+    }
+}
