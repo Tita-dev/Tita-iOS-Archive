8b43903a228f77957d6da27382b616b18b17d4ca
hyein0112 <bhi12134@gmail.com>
2022-01-27 18:33:20 +0900

    [CREATE] logo

--- Tita/Views/SignUpDescriptionView.swift
+++ Tita/Views/SignUpDescriptionView.swift
@@ -12,6 +12,10 @@ import Then
 class SignUpDescriptionView: UIView {
     private let viewBounds = UIScreen.main.bounds
 
+    private let logo = UIImageView().then {
+        $0.image = UIImage(named: "Tita-Logo")
+    }
+    
 // MARK: - LifeCycles
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,11 +39,16 @@ class SignUpDescriptionView: UIView {
         
     // MARK: - addView
     private func addView(){
-        [].forEach { addSubview($0) }
+        [logo].forEach { addSubview($0) }
     }
             
     // MARK: - location
     private func location(){
-
+        logo.snp.makeConstraints { make in
+            make.width.equalTo(viewBounds.width/3.22)
+            make.height.equalTo(viewBounds.height/19.33)
+            make.top.equalToSuperview()
+            make.left.equalTo(viewBounds.width/15.63)
+        }
     }
 }
