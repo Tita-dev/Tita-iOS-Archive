297bf3bfc7efef9b34662e7f8a66b4d6b9ee4e36
hyein0112 <bhi12134@gmail.com>
2022-01-28 22:37:46 +0900

    [CREATE] BirthDateViewController

---
+++ Tita/Controllers/BirthDateViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BirthDateViewController.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/01/28.
+//
+
+import UIKit
+
+class BirthDateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
