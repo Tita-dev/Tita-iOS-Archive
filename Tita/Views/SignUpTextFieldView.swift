ff28bde446158fad28ed191416d0899f75e472b1
hyein0112 <bhi12134@gmail.com>
2022-01-30 00:36:35 +0900

    [CREATE] SignUpTextFieldView

---
+++ Tita/Views/SignUpTextFieldView.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpTextFieldView.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/01/30.
+//
+
+import UIKit
+
+class SignUpTextFieldView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
