587394dc50958607c1dc30f2774396aa2e6da1f7
hyein0112 <bhi12134@gmail.com>
2022-02-01 23:42:18 +0900

    [UPDATE] SignUpSchoolViewController 기본세팅

--- Tita/Controllers/SignUpSchoolViewController.swift
+++ Tita/Controllers/SignUpSchoolViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SignUpSchoolViewController: UIViewController {
-
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
     }
-    */
-
+    
+    // MARK: - Add View
+    private func addView(){
+        
+    }
+    
+    // MARK: - Corner Radius
+    private func cornerRadius(){
+        
+    }
+    
+    // MARK: - Location
+    private func location(){
+        
+    }
+    
 }
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct SignUpSchoolViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        SignUpSchoolViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct SignUpSchoolViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SignUpSchoolViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
