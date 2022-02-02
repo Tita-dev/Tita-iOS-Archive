e439bf2bcd95f287d0097130c1597061a3a58f07
hyein0112 <bhi12134@gmail.com>
2022-02-03 03:49:27 +0900

    [CREATE] EndSignUpViewController

---
+++ Tita/Controllers/EndSignUpViewController.swift
@@ -0,0 +1,72 @@
+//
+//  EndSignUpViewController.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/02/03.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class EndSignUpViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
+    }
+    
+    // MARK: - Add View
+    private func addView(){
+        
+    }
+    
+    // MARK: - Corner Radius
+    private func cornerRadius(){
+        
+    }
+    
+    // MARK: - Location
+    private func location(){
+        
+    }
+    
+}
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct EndSignUpViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        EndSignUpViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct EndSignUpViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            EndSignUpViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
