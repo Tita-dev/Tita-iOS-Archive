830d700eb2899bf5ad29e67e9dcd120324f4ade5
hyein0112 <bhi12134@gmail.com>
2022-03-02 20:04:17 +0900

    [CREATE] CafeteriaViewController

---
+++ Tita/Controllers/CafeteriaViewController.swift
@@ -0,0 +1,71 @@
+//
+//  CafeteriaViewController.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/03/02.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class CafeteriaViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
+    }
+    
+    // MARK: - Add View
+    private func addView(){
+        
+    }
+    
+    // MARK: - Corner Radius
+    private func cornerRadius(){
+        
+    }
+    
+    // MARK: - Location
+    private func location(){
+        
+    }
+}
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct CafeteriaViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        CafeteriaViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            CafeteriaViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
