9c4dcde65a3899befcced222a5c8d9027642d2a8
백혜인 <81304598+hyein0112@users.noreply.github.com>
2022-03-02 10:14:33 +0900

    [DELETE] Preview 코드 삭제

--- Tita/Controllers/BirthDateViewController.swift
+++ Tita/Controllers/BirthDateViewController.swift
@@ -174,29 +174,3 @@ class BirthDateViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     }
     
 }
-
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct BirthDateViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        BirthDateViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct BirthDateViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            BirthDateViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
