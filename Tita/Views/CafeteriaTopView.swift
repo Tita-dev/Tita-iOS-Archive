3cf5ad6d35258a8db2b76adbfc0c0a005fd2d295
hyein0112 <bhi12134@gmail.com>
2022-03-02 20:20:45 +0900

    [CREATE] previousButton 추가

--- Tita/Views/CafeteriaTopView.swift
+++ Tita/Views/CafeteriaTopView.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class CafeteriaTopView: UIView {
     private let viewBounds = UIScreen.main.bounds
+    
+    let previousButton = UIButton().then {
+       $0.setImage(UIImage(named: "Tita-Previous"), for: .normal)
+    }
+
 
 // MARK: - LifeCycles
     override init(frame: CGRect) {
@@ -33,11 +38,16 @@ class CafeteriaTopView: UIView {
         
     // MARK: - addView
     private func addView(){
-        [].forEach { addSubview($0) }
+        [previousButton].forEach { addSubview($0) }
     }
             
     // MARK: - location
     private func location(){
-
+        previousButton.snp.makeConstraints { make in
+            make.width.equalTo(viewBounds.width/17.86)
+            make.height.equalTo(viewBounds.height/40.6)
+            make.top.equalToSuperview()
+            make.left.equalTo(viewBounds.width/15.63)
+        }
     }
 }
