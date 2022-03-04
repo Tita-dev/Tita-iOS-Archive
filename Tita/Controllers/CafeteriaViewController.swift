4da1ff9b5892eba7ef00c3424a9ead339147e14e
백혜인 <81304598+hyein0112@users.noreply.github.com>
2022-03-04 15:52:15 +0900

    Update Tita/Controllers/CafeteriaViewController.swift
    Co-authored-by: 노연주 <yjnoh1016@naver.com>

--- Tita/Controllers/CafeteriaViewController.swift
+++ Tita/Controllers/CafeteriaViewController.swift
@@ -108,28 +108,3 @@ class CafeteriaViewController: UIViewController {
     }
 }
 
-//MARK: - Preview
-#if DEBUG
-import SwiftUI
-struct CafeteriaViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        CafeteriaViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            CafeteriaViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
