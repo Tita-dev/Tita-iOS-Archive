6bdd1fdebcfa00ea7297c4de707f8a534be1fee6
hyein0112 <bhi12134@gmail.com>
2022-03-12 23:44:35 +0900

    [UPDATE] FindAccountButton에 leftPadding 추가

--- Tita/Views/Buttons/FindAccountButton.swift
+++ Tita/Views/Buttons/FindAccountButton.swift
@@ -29,6 +29,8 @@ class FindAccountButton: UIButton {
         self.layer.cornerRadius = UIScreen.main.bounds.width/37.5
         self.layer.borderWidth = UIScreen.main.bounds.width/375
         self.layer.borderColor = UIColor.rgb(red: 205, green: 205, blue: 205).cgColor
+        self.contentHorizontalAlignment = .left
+        self.addLeftPadding(20)
         self.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-Light")
         self.setTitleColor(.rgb(red: 133, green: 133, blue: 133), for: .normal)
     }
