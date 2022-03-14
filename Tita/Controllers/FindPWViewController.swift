a2bb052fdd4755343df8217cbc07e7c7ff8e036e
hyein0112 <bhi12134@gmail.com>
2022-03-15 08:36:14 +0900

    [CREATE] textFieldView

--- Tita/Controllers/FindPWViewController.swift
+++ Tita/Controllers/FindPWViewController.swift
@@ -16,6 +16,11 @@ class FindPWViewController: UIViewController {
         $0.previousButton.addTarget(self, action: #selector(tapPrevious(_:)), for: .touchUpInside)
     }
     
+    private let textFieldView = SignUpTextFieldView().then {
+        $0.dataSetting(labelText: "이메일")
+    }
+
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,7 +43,7 @@ class FindPWViewController: UIViewController {
     
     // MARK: - Add View
     private func addView(){
-        [descriptionView].forEach { view.addSubview($0) }
+        [descriptionView, textFieldView].forEach { view.addSubview($0) }
     }
     
     // MARK: - Corner Radius
@@ -52,6 +57,13 @@ class FindPWViewController: UIViewController {
             make.width.equalToSuperview()
             make.height.equalToSuperview().dividedBy(3.98)
             make.top.equalToSuperview().offset(self.view.frame.height/13.1)
+            make.centerX.equalToSuperview()
+        }
+        
+        textFieldView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(9.12)
+            make.top.equalTo(descriptionView.snp.bottom).offset(self.view.frame.height/32.48)
             make.centerX.equalToSuperview()
         }
     }
