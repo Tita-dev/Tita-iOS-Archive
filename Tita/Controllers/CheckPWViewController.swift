594d0d833553fa2604d1a49691cec2d3b7bd1b94
hyein0112 <bhi12134@gmail.com>
2022-03-15 19:58:06 +0900

    [UPDATE] CheckPWViewController

--- Tita/Controllers/CheckPWViewController.swift
+++ Tita/Controllers/CheckPWViewController.swift
@@ -6,24 +6,66 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class CheckPWViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    //MARK: - Properties
+    private let descriptionView = SignUpDescriptionView().then {
+        $0.dataSetting(description: "비밀번호를 재설정했어요!\n이제 다시 로그인을 시도해 봐요!", additionalDescription: nil)
+        $0.previousButton.addTarget(self, action: #selector(tapPrevious(_:)), for: .touchUpInside)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let mainButton = LoginButton().then {
+        $0.dataSetting(title: "메인으로")
+        $0.addTarget(self, action: #selector(tapMainButton(_:)), for: .touchUpInside)
     }
-    */
-
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    //MARK: - Selectors
+    @objc private func tapPrevious(_ sender: UIButton){
+        navigationController?.popViewController(animated: true)
+        print("previous")
+    }
+    
+    @objc private func tapMainButton(_ sender: UIButton){
+        let nextVC = LoginViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+       print("complete sign up")
+    }
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        location()
+    }
+    
+    // MARK: - Add View
+    private func addView(){
+        [descriptionView, mainButton].forEach { view.addSubview($0)}
+    }
+    
+    // MARK: - Location
+    private func location(){
+        descriptionView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(3.37)
+            make.top.equalToSuperview().offset(self.view.frame.height/13.1)
+            make.centerX.equalToSuperview()
+        }
+        
+        mainButton.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(16.24)
+            make.bottom.equalToSuperview().inset(self.view.frame.height/25.38)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
 }
