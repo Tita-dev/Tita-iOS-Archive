eb6ebc9af84a9a59254d45243f9780b0179976ed
노연주 <yjnoh1016@naver.com>
2022-03-18 09:05:40 +0900

    Revert "Revert "Develop""

---
+++ Tita/Controllers/SignUpIDViewController.swift
@@ -0,0 +1,114 @@
+//
+//  SignUpIDViewController.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/01/29.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class SignUpIDViewController: UIViewController{
+    //MARK: - Properties
+    private let descriptionView = DescriptionView().then {
+        $0.dataSetting(description: "본격적으로 계정을 만들어봐요!", additionalDescription: "먼저, 아이디부터 입력할까요?")
+        $0.previousButton.addTarget(self, action: #selector(tapPrevious(_:)), for: .touchUpInside)
+    }
+    
+    private let textFieldView = TextFieldView().then {
+        $0.dataSetting(labelText: "아이디")
+    }
+    
+    private let nextButton = LoginButton().then {
+        $0.dataSetting(title: "다음")
+        $0.addTarget(self, action: #selector(tapNextButton(_:)), for: .touchUpInside)
+    }
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    //MARK: - Selectors
+    @objc private func tapPrevious(_ sender: UIButton){
+        navigationController?.popViewController(animated: true)
+        print("previous")
+    }
+    
+    @objc private func tapNextButton(_ sender: UIButton){
+        print("next")
+        let nextVC = SignUpPWViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
+    }
+    
+    //MARK: - Keyboard Setting
+    @objc func keyboardWillShow(_ sender: Notification) {
+        nextButton.snp.remakeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(16.24)
+            make.top.equalToSuperview().offset(self.view.frame.height/1.8)
+            make.centerX.equalToSuperview()
+        }
+
+    }
+
+    @objc func keyboardWillHide(_ sender: Notification) {
+        nextButton.snp.remakeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(16.24)
+            make.bottom.equalToSuperview().inset(self.view.frame.height/25.38)
+            make.centerX.equalToSuperview()
+        }
+        
+    }
+        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textFieldView.textField.resignFirstResponder()
+    }
+    
+    //MARK: - NotificationCenterSetting
+    private func notificationCenterSetting(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name:UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        location()
+        notificationCenterSetting()
+    }
+    
+    // MARK: - Add View
+    private func addView(){
+        [descriptionView, textFieldView, nextButton].forEach { view.addSubview($0)}
+    }
+
+    // MARK: - Location
+    private func location(){
+        descriptionView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(3.98)
+            make.top.equalToSuperview().offset(self.view.frame.height/13.1)
+            make.centerX.equalToSuperview()
+        }
+        
+        textFieldView.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(9.12)
+            make.top.equalTo(descriptionView.snp.bottom).offset(self.view.frame.height/32.48)
+            make.centerX.equalToSuperview()
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(16.24)
+            make.bottom.equalToSuperview().inset(self.view.frame.height/25.38)
+            make.centerX.equalToSuperview()
+        }
+    }
+    
+}
---
+++ Tita/Views/CafeteriaDateView.swift
@@ -0,0 +1,69 @@
+//
+//  CafeteriaDateView.swift
+//  Tita
+//
+//  Created by 혜인 on 2022/03/03.
+//
+
+import UIKit
+
+class CafeteriaDateView: UIView {
+    private let viewBounds = UIScreen.main.bounds
+    
+    let lastDayButton = UIButton().then {
+        $0.setImage(UIImage(named: "Tita-ChevronDown_left"), for: .normal)
+    }
+    
+    let dateLabel = UILabel().then {
+        $0.dynamicFont(fontSize: 20, currentFontName: "AppleSDGothicNeo-Bold")
+    }
+    
+    let nextDayButton = UIButton().then {
+        $0.setImage(UIImage(named: "TIta-ChevronDown_right"), for: .normal)
+    }
+
+// MARK: - LifeCycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+            
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+        
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        configureUI()
+    }
+        
+    // MARK: - Helpers
+    private func configureUI(){
+        addView()
+        location()
+    }
+        
+    // MARK: - addView
+    private func addView(){
+        [lastDayButton, dateLabel, nextDayButton].forEach { addSubview($0) }
+    }
+            
+    // MARK: - location
+    private func location(){
+        lastDayButton.snp.makeConstraints { make in
+            make.width.equalTo(viewBounds.width/12.5)
+            make.height.equalTo(viewBounds.height/27.01)
+            make.left.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        nextDayButton.snp.makeConstraints { make in
+            make.width.height.centerY.equalTo(lastDayButton)
+            make.right.equalToSuperview()
+        }
+    }
+}
